22505562fd1bd6c1392cf643b0823922edca2030
henryzhang03 <henryzhang1719@gmail.com>
2022-01-19 16:34:29 -0800

    fix2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,26 +4,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
+        // || markdown.charAt(currentIndex - 1) == '('
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+
             if(nextOpenBracket == -1) {
                 break;
             }
+
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+
+            if(markdown.charAt(closeParen - 1) == '(') {
+                closeParen = markdown.indexOf(")", closeParen + 1);
+            }
+
             if(nextCloseBracket + 1 == openParen) {
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
+
             currentIndex = closeParen + 1;
-            System.out.println("Current index is: " + currentIndex);
         }
         return toReturn;
     }
