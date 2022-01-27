3c5a9aa82da26e684ce87ae74a0752beff2527dc
henryzhang03 <henryzhang1719@gmail.com>
2022-01-26 16:43:35 -0800

    Made all the prof's md files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
@@ -6,4 +11,34 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testOriginalFile() throws IOException {
+        Path fileName = Path.of("originalbreak.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testBreakFile1() throws IOException {
+        Path fileName = Path.of("break1.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testBreakFile2() throws IOException {
+        Path fileName = Path.of("break2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something().com"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testBreakFile3() throws IOException {
+        Path fileName = Path.of("break3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
+
+    
 }
